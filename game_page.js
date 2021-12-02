5cbecda172b6ca77745af818e0ae51f8ecd6619f
Zhalhou1-dev <83404810+Zhalhou1-dev@users.noreply.github.com>
2021-12-02 08:43:46 +0530

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -1,5 +1,5 @@
 function send(){
-    number1=docuement.getElementById("num1").value;
+    number1=document.getElementById("num1").value;
     number2=document.getElementById("num2").value;
     actual_answer=parseInt(number1)*parseInt(number2);
     question_number="<h4>"+number1+" X "+number2+"</h4>";
