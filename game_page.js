c354e10827de44d23b8ad6792743de42e23f3469
Zhalhou1-dev <83404810+Zhalhou1-dev@users.noreply.github.com>
2021-12-02 09:46:58 +0530

    Update game_page.js

--- game_page.js
+++ game_page.js
@@ -1,3 +1,5 @@
+question_turn="player1";
+answer_turn="player2";
 function send(){
     number1=document.getElementById("num1").value;
     number2=document.getElementById("num2").value;
@@ -11,4 +13,18 @@ function send(){
     docuement.getElementById("num1").innerHTML="";
     docuement.getElementById("num2").innerHTML="";
 
+}
+function check(){
+    get_answer=document.getElementById("input_check_box").value;
+    if(get_answer==actual_answer){
+        if (answer_turn=="player1"){
+            update_player1_score=player1score+1;
+            document.getElementById("player1_score").innerHTML=update_player1_score;
+        }
+        else{
+            update_player2_score=player2_score;
+            document.getElementById("player2_score").innerHTML=update_player2_score;
+        
+        }
+    }
 }
